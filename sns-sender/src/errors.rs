ba7b744c562facc598b653f7cb64494396adb956
Joshua Mo <joshua.mo.876@gmail.com>
2024-03-20 14:48:21 +0000

    refactor: fmt/clippy, warning cleanup

--- sns-sender/src/errors.rs
+++ sns-sender/src/errors.rs
@@ -5,39 +5,16 @@ use axum::{
 use thiserror::Error;
 
 use aws_sdk_sns::error::SdkError;
-use aws_sdk_sns::operation::create_topic::CreateTopicError;
-use aws_sdk_sns::operation::delete_topic::DeleteTopicError;
 use aws_sdk_sns::operation::publish::PublishError;
-use aws_sdk_sns::operation::subscribe::SubscribeError;
-use aws_sdk_sns::operation::unsubscribe::UnsubscribeError;
 
 #[derive(Debug, Error)]
 pub enum ApiError {
-    #[error("SQL error: {0}")]
-    SQL(#[from] sqlx::Error),
-    #[error("Error while creating topic: {0}")]
-    CreateTopic(#[from] SdkError<CreateTopicError>),
-    #[error("Error while deleting topic: {0}")]
-    DeleteTopic(#[from] SdkError<DeleteTopicError>),
-    #[error("Error while subscribing to topic: {0}")]
-    SubscribeToTopic(#[from] SdkError<SubscribeError>),
-    #[error("Error while unsubscribing from topic: {0}")]
-    UnsubscribeFromTopic(#[from] SdkError<UnsubscribeError>),
     #[error("Error while publishing message: {0}")]
     PublishMessage(#[from] SdkError<PublishError>),
 }
 
 impl IntoResponse for ApiError {
     fn into_response(self) -> Response {
-        let res = match self {
-            Self::SQL(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
-            Self::CreateTopic(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
-            Self::DeleteTopic(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
-            Self::SubscribeToTopic(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
-            Self::UnsubscribeFromTopic(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
-            Self::PublishMessage(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
-        };
-
-        res.into_response()
+        (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()).into_response()
     }
 }
--- sns-sender/src/main.rs
+++ sns-sender/src/main.rs
@@ -1,11 +1,10 @@
-use axum::{routing::get, Router, Json, extract::State, http::StatusCode, response::IntoResponse};
-use aws_config::{Region};
-use aws_sdk_sns::types::Tag;
+use anyhow::Result as AnyhowResult;
+use aws_config::Region;
 use aws_credential_types::Credentials;
-use shuttle_secrets::SecretStore;
-use sqlx::PgPool;
+use aws_sdk_sns::Client;
+use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::get, Json, Router};
 use serde::Deserialize;
-use std::collections::HashMap;
+use shuttle_secrets::SecretStore;
 
 mod errors;
 use errors::ApiError;
@@ -13,7 +12,7 @@ use errors::ApiError;
 #[derive(Clone)]
 pub struct AppState {
     sns: aws_sdk_sns::Client,
-    db: PgPool,
+    topic_arn: String,
 }
 
 async fn hello_world() -> &'static str {
@@ -21,143 +20,103 @@ async fn hello_world() -> &'static str {
 }
 
 #[shuttle_runtime::main]
-async fn main(
-    #[shuttle_secrets::Secrets] secrets: SecretStore,
-    #[shuttle_shared_db::Postgres] db: PgPool,
-) -> shuttle_axum::ShuttleAxum {
+async fn main(#[shuttle_secrets::Secrets] secrets: SecretStore) -> shuttle_axum::ShuttleAxum {
+    let access_key_id = secrets
+        .get("AWS_ACCESS_KEY_ID")
+        .expect("AWS_ACCESS_KEY_ID not set in Secrets.toml");
+    let secret_access_key = secrets
+        .get("AWS_SECRET_ACCESS_KEY")
+        .expect("AWS_ACCESS_KEY_ID not set in Secrets.toml");
 
-    let access_key_id = secrets.get("AWS_ACCESS_KEY_ID").expect("AWS_ACCESS_KEY_ID not set in Secrets.toml");
-    let secret_access_key = secrets.get("AWS_SECRET_ACCESS_KEY").expect("AWS_ACCESS_KEY_ID not set in Secrets.toml");
+    let creds = Credentials::from_keys(access_key_id, secret_access_key, None);
 
-        let creds = Credentials::from_keys(
-               access_key_id,
-               secret_access_key,
-               None
-            );
+    let cfg = aws_config::from_env()
+        .region(Region::new("eu-west-02"))
+        .credentials_provider(creds)
+        .load()
+        .await;
 
-        let cfg = aws_config::from_env()
-            .region(Region::new("eu-west-02"))
-            .credentials_provider(creds)
-            .load()
-            .await;
+    let sns = aws_sdk_sns::Client::new(&cfg);
+    let topic_arn = create_topic(&sns, "my_topic").await?;
 
-       let sns = aws_sdk_sns::Client::new(&cfg);
+    // NOTE: Change this to your deployment URL for your receiver service!
+    // The SNS receiver route should be on `/sns` as instructed previously
+    let url = "https://sns-receiver.shuttleapp.rs/sns";
 
-    let state = AppState { sns, db };
+    if !subscription_exists(&sns, url, &topic_arn).await? {
+        subscribe_to_topic(&sns, url, &topic_arn).await?;
+    }
+
+    let state = AppState { sns, topic_arn };
 
     let router = Router::new().route("/", get(hello_world)).with_state(state);
 
     Ok(router.into())
 }
 
-#[derive(Deserialize)]
-pub struct CreateTopicParams {
-    name: String,
-    tags: Option<Vec<(String, String)>>,
-    attributes: Option<HashMap<String, String>>
+async fn subscription_exists(sns: &Client, url: &str, arn: &str) -> AnyhowResult<bool> {
+    let subscribers = sns
+        .list_subscriptions()
+        .send()
+        .await
+        .map_err(|e| anyhow::anyhow!("error: {e}"))?;
+
+    if let Some(subs) = subscribers.subscriptions {
+        if subs.iter().any(|sub| {
+            sub.clone().endpoint.unwrap() == *url && sub.clone().topic_arn.unwrap() == *arn
+        }) {
+            return Ok(true);
+        }
+
+        return Ok(false);
+    }
+
+    Ok(false)
 }
 
-async fn create_topic(
-    State(state): State<AppState>,
-    Json(json): Json<CreateTopicParams>
-)-> Result<impl IntoResponse, ApiError> {
-    let topic = state.sns.create_topic();
+async fn create_topic(sns: &Client, name: &str) -> AnyhowResult<String> {
+    let topic = sns.create_topic().name(name);
 
-    let topic = topic.name(json.name);
-
-    let tags = match json.tags {
-        Some(tags) => {
-            let res: Vec<Tag> = tags.into_iter().map(|(key, value)| {
-
-            Tag::builder()
-                .set_key(Some(key))
-                .set_value(Some(value))
-                .build().unwrap()
-
-            }).collect();
-
-            Some(res)
-
-        },
-        None => None
-    };
-
-    let topic = if tags.is_some() { topic.set_tags(tags) } else { topic };
-    let topic = if json.attributes.is_some() { topic.set_attributes(json.attributes) } else { topic };
-    let output = topic.send().await?;
+    let output = topic.send().await.unwrap();
 
     println!("Topic created: {output:?}");
-
-    Ok(StatusCode::OK)
+    Ok(output.topic_arn.unwrap())
 }
 
-async fn delete_topic(
-    State(state): State<AppState>,
-    Json(json): Json<DeleteItemParams>
-) -> Result<impl IntoResponse, ApiError> {
-    let _ = state.sns.delete_topic()
-        .topic_arn(json.arn)
-        .send().await?;
+async fn subscribe_to_topic(sns: &Client, url: &str, arn: &str) -> AnyhowResult<()> {
+    let sub = sns
+        .subscribe()
+        .protocol("https".to_string())
+        .endpoint(url)
+        .topic_arn(arn);
 
-    Ok(StatusCode::OK)
-}
-
-#[derive(Deserialize)]
-pub struct EmailSubscriptionParams {
-    email: String,
-    arn: String,
-    attributes: Option<HashMap<String, String>>
-}
-
-#[derive(Deserialize)]
-pub struct DeleteItemParams {
-    arn: String
-}
-
-async fn subscribe_to_topic(
-    State(state): State<AppState>,
-    Json(json): Json<EmailSubscriptionParams>
-) -> Result<impl IntoResponse, ApiError> {
-    let sub = state.sns.subscribe()
-        .protocol("email".to_string())
-        .endpoint(json.email)
-        .topic_arn(json.arn);
-
-    let sub = if json.attributes.is_some() { sub.set_attributes(json.attributes) } else { sub };
-
-    let output = sub.send().await?;
+    let output = sub
+        .send()
+        .await
+        .map_err(|e| anyhow::anyhow!("error: {e}"))?;
     println!("New subscriber created: {output:?}");
 
-    Ok(StatusCode::OK)
-}
-
-async fn unsubscribe_from_topic(
-    State(state): State<AppState>,
-    Json(json): Json<DeleteItemParams>
-) -> Result<impl IntoResponse, ApiError> {
-    let _ = state.sns.unsubscribe()
-        .subscription_arn(json.arn)
-        .send().await?;
-
-    Ok(StatusCode::OK)
+    Ok(())
 }
 
 #[derive(Deserialize)]
 struct PublishMessageParams {
-    arn: String,
     message: String,
-    subject: String
+    subject: String,
 }
 
 async fn publish_message(
     State(state): State<AppState>,
-    Json(json): Json<PublishMessageParams>
+    Json(json): Json<PublishMessageParams>,
 ) -> Result<impl IntoResponse, ApiError> {
-    let res = state.sns.publish()
-        .topic_arn(json.arn)
+    state
+        .sns
+        .publish()
+        .topic_arn(state.topic_arn)
         .message(json.message)
         .subject(json.subject)
-        .send().await?;
+        .send()
+        .await?;
 
     Ok(StatusCode::OK)
 }
